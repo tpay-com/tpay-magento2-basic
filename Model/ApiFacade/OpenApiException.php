2faa9025aaf442e24f4424c1a1d146f2c9da34ef
Adam <adam.wysocki6431@gmail.com>
2024-04-25 10:26:11 +0200

    Fix imports

--- Model/ApiFacade/OpenApiException.php
+++ Model/ApiFacade/OpenApiException.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Tpay\Magento2\Model\ApiFacade;
 
+use Exception;
+use Throwable;
+
 class OpenApiException extends Exception
 {
     public function __construct($message = '', $code = 0, ?Throwable $previous = null)
