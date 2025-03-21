378fd5b1e9a229d059244f7f181617cc10d7beea
Kamil Balwierz <kamil.balwierz@tpay.com>
2025-03-21 15:56:10 +0100

    Proper failed notification format

--- Controller/Tpay/Notification.php
+++ Controller/Tpay/Notification.php
@@ -4,39 +4,48 @@ declare(strict_types=1);
 
 namespace Tpay\Magento2\Controller\Tpay;
 
-use Laminas\Http\Response;
+use Magento\Framework\App\Action\HttpPostActionInterface;
 use Magento\Framework\App\CsrfAwareActionInterface;
 use Magento\Framework\App\Request\InvalidRequestException;
 use Magento\Framework\App\RequestInterface;
-use Magento\Framework\App\ResponseInterface;
+use Magento\Framework\Controller\Result\Raw;
+use Magento\Framework\Controller\Result\RawFactory;
 use Throwable;
 use Tpay\Magento2\Notification\NotificationProcessor;
 
-class Notification implements CsrfAwareActionInterface
+class Notification implements CsrfAwareActionInterface, HttpPostActionInterface
 {
+    const BAD_REQUEST = 400;
+    const HTTP_OK = 200;
+
     /** @var NotificationProcessor */
     protected $notificationProcessor;
 
-    /** @var ResponseInterface */
-    private $response;
+    /** @var RawFactory */
+    private $resultPageFactory;
 
     public function __construct(
-        ResponseInterface $response,
+        RawFactory $resultPageFactory,
         NotificationProcessor $notificationProcessor
     ) {
-        $this->response = $response;
+        $this->resultPageFactory = $resultPageFactory;
         $this->notificationProcessor = $notificationProcessor;
     }
 
-    public function execute(): ?Response
+    public function execute(): Raw
     {
+        $result = $this->resultPageFactory->create();
         try {
             $this->notificationProcessor->process();
         } catch (Throwable $e) {
-            return $this->response->setStatusCode(Response::STATUS_CODE_400)->setContent($e->getMessage());
+            return $result
+                ->setHttpResponseCode(self::BAD_REQUEST)
+                ->setContents('FALSE - ' . $e->getMessage());
         }
 
-        return $this->response->setStatusCode(Response::STATUS_CODE_200)->setContent('TRUE');
+        return $result
+            ->setHttpResponseCode(self::HTTP_OK)
+            ->setContents('TRUE');
     }
 
     public function createCsrfValidationException(RequestInterface $request): ?InvalidRequestException
