087a3be97f766fc084e0d5fda78093ebfad796fb
Kamil Balwierz <kamil.balwierz@tpay.com>
2025-04-01 16:25:30 +0200

    Introduces FieldSet banner in adminhtml

---
+++ Block/System/Config/TpayPaymentFieldset.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tpay\Magento2\Block\System\Config;
+
+use Magento\Backend\Block\Template;
+use Magento\Config\Block\System\Config\Form\Fieldset;
+
+class TpayPaymentFieldset extends Fieldset
+{
+    protected function _getHeaderCommentHtml($element)
+    {
+        $block = $this->getLayout()->createBlock(Template::class)->setTemplate('Tpay_Magento2::system/config/header.phtml');
+        $registrationUrl = $this->_scopeConfig->getValue('payment/tpaycom_magento2basic/registration_override');
+        if (empty($registrationUrl)) {
+            $registrationUrl = 'https://register.tpay.com/';
+        }
+        $block->setData('registration_url', $registrationUrl);
+        return $block->toHtml();
+    }
+}
