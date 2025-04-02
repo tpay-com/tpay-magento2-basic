17d464d542305eb61b9507e347eea5df5060c4f8
Kamil Balwierz <kamil.balwierz@tpay.com>
2025-04-02 14:05:10 +0200

    Adds info banner in payment configuration (#77)

---
+++ Block/System/Config/TpayPaymentFieldset.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tpay\Magento2\Block\System\Config;
+
+use Magento\Backend\Block\Template;
+use Magento\Config\Block\System\Config\Form\Fieldset;
+
+class TpayPaymentFieldset extends Fieldset
+{
+    // phpcs:ignore
+    protected function _getHeaderCommentHtml($element)
+    {
+        $block = $this->getLayout()->createBlock(Template::class)->setTemplate('Tpay_Magento2::system/config/header.phtml');
+        $registrationUrl = $this->_scopeConfig->getValue('payment/tpaycom_magento2basic/registration_override');
+        if (empty($registrationUrl)) {
+            $registrationUrl = 'https://register.tpay.com/';
+        }
+        $block->setData('registration_url', $registrationUrl);
+
+        return $block->toHtml();
+    }
+}
