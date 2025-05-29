e50bc694da8393cf2cf64184e662750d2b8f164e
Kamil Balwierz <kamil.balwierz@tpay.com>
2025-05-29 11:28:57 +0200

    Resolves DI compilation issue on Magento 2.4.8+ (#86)
    Due to magento/magento2#33161 since Magento 2.4.8 SDK must be declared as library if it is to be used in dependency injection definitions

--- registration.php
+++ registration.php
@@ -5,3 +5,16 @@ Magento\Framework\Component\ComponentRegistrar::register(
     'Tpay_Magento2',
     __DIR__
 );
+
+foreach (Composer\Autoload\ClassLoader::getRegisteredLoaders() as $loader) {
+    $path = $loader->findFile(Tpay\OpenApi\Api\TpayApi::class);
+    if ($path) {
+        Magento\Framework\Component\ComponentRegistrar::register(
+            Magento\Framework\Component\ComponentRegistrar::LIBRARY,
+            'tpay-com/tpay-openapi-php',
+            dirname($path, 2)
+        );
+
+        return;
+    }
+}
