50ad409ec98ed499d570fde02a5395852486a3f7
Kamil Balwierz <kamil.balwierz@tpay.com>
2025-09-29 12:31:26 +0200

    Magento Coding Standard: reworked resolving installed versions of dependencies - avoid using require

--- registration.php
+++ registration.php
@@ -1,20 +1,30 @@
 <?php
 
+use Composer\InstalledVersions;
+
 Magento\Framework\Component\ComponentRegistrar::register(
     Magento\Framework\Component\ComponentRegistrar::MODULE,
     'Tpay_Magento2',
     __DIR__
 );
 
-foreach (Composer\Autoload\ClassLoader::getRegisteredLoaders() as $loader) {
-    $path = $loader->findFile(Tpay\OpenApi\Api\TpayApi::class);
-    if ($path) {
-        Magento\Framework\Component\ComponentRegistrar::register(
-            Magento\Framework\Component\ComponentRegistrar::LIBRARY,
-            'tpay-com/tpay-openapi-php',
-            dirname($path, 2)
-        );
+if (class_exists(InstalledVersions::class)) {
+    Magento\Framework\Component\ComponentRegistrar::register(
+        Magento\Framework\Component\ComponentRegistrar::LIBRARY,
+        'tpay-com/tpay-openapi-php',
+        InstalledVersions::getInstallPath('tpay-com/tpay-openapi-php')
+    );
+} else {
+    foreach (Composer\Autoload\ClassLoader::getRegisteredLoaders() as $loader) {
+        $path = $loader->findFile(Tpay\OpenApi\Api\TpayApi::class);
+        if ($path) {
+            Magento\Framework\Component\ComponentRegistrar::register(
+                Magento\Framework\Component\ComponentRegistrar::LIBRARY,
+                'tpay-com/tpay-openapi-php',
+                dirname($path, 2)
+            );
 
-        return;
+            return;
+        }
     }
 }
