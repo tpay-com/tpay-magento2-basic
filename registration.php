447fdb378af3b27be432cdc5f60de1d4fe541397
Kamil Balwierz <kamil.balwierz@tpay.com>
2025-10-02 10:54:19 +0200

    remove depenency on composer autoloadres

--- registration.php
+++ registration.php
@@ -2,29 +2,21 @@
 
 use Composer\InstalledVersions;
 
-Magento\Framework\Component\ComponentRegistrar::register(
-    Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Tpay_Magento2',
-    __DIR__
-);
-
 if (class_exists(InstalledVersions::class)) {
-    Magento\Framework\Component\ComponentRegistrar::register(
-        Magento\Framework\Component\ComponentRegistrar::LIBRARY,
-        'tpay-com/tpay-openapi-php',
-        InstalledVersions::getInstallPath('tpay-com/tpay-openapi-php').'/src'
-    );
-} else {
-    foreach (Composer\Autoload\ClassLoader::getRegisteredLoaders() as $loader) {
-        $path = $loader->findFile(Tpay\OpenApi\Api\TpayApi::class);
-        if ($path) {
-            Magento\Framework\Component\ComponentRegistrar::register(
-                Magento\Framework\Component\ComponentRegistrar::LIBRARY,
-                'tpay-com/tpay-openapi-php',
-                dirname($path, 2)
-            );
+    try {
+        $path = InstalledVersions::getInstallPath('tpay-com/tpay-openapi-php');
 
-            return;
-        }
+        Magento\Framework\Component\ComponentRegistrar::register(
+            Magento\Framework\Component\ComponentRegistrar::MODULE,
+            'Tpay_Magento2',
+            __DIR__
+        );
+
+        Magento\Framework\Component\ComponentRegistrar::register(
+            Magento\Framework\Component\ComponentRegistrar::LIBRARY,
+            'tpay-com/tpay-openapi-php',
+            $path . '/src'
+        );
+    } catch (OutOfBoundsException $e) {
     }
 }
