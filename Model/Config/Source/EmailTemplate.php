a7dabab39c59b17c180811586bf06837ce5bdc71
Kamil Balwierz <kamil.balwierz@tpay.com>
2026-06-03 10:22:29 +0200

    documentation

--- Model/Config/Source/EmailTemplate.php
+++ Model/Config/Source/EmailTemplate.php
@@ -4,6 +4,15 @@ namespace Tpay\Magento2\Model\Config\Source;
 
 class EmailTemplate extends \Magento\Config\Model\Config\Source\Email\Template
 {
+    /**
+     * This method is overrided because "merchant location" selector dynamically
+     * changes first part of path (eg. payment_us, payment_other) based on
+     * selected country - making internals of base logic Email Template Source
+     * fail to find default email template based on exact match of configuration
+     * option path (as original value starts with payment)
+     *
+     * @return string
+     */
     public function getPath()
     {
         $path = parent::getPath();
